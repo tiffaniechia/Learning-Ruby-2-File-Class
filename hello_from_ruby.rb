a88756fc2946e4f55d89bb148fdbde0a4443ae61
tiffanie <yt.chia.2009@smu.edu.sg>
2014-02-24 19:06:23 +0000

    challenge 3

--- hello_from_ruby.rb
+++ hello_from_ruby.rb
@@ -1,2 +1,11 @@
-#Challenge 1
-File.new('hellofromruby.txt')
+#challenge 3
+puts "What is happening"
+@ask_for_input = gets.chomp
+puts "#{@ask_for_input}"
+save_input
+
+def save_input 
+	file = File.open("hellofromruby.txt", "w")
+	file.puts ("#{@ask_for_input}")
+end
+
