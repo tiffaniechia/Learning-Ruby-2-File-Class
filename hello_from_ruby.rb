42d5631885642ce7c9fa16624422b607c5335383
tiffanie <yt.chia.2009@smu.edu.sg>
2014-02-24 19:53:04 +0000

    challenge 2,3,4,5

--- hello_from_ruby.rb
+++ hello_from_ruby.rb
@@ -2,10 +2,29 @@
 puts "What is happening"
 @ask_for_input = gets.chomp
 puts "#{@ask_for_input}"
-save_input
 
-def save_input 
-	file = File.open("hellofromruby.txt", "w")
-	file.puts ("#{@ask_for_input}")
+puts "Please enter 'save' to save your input to the file"
+puts "Please enter 'read' to read content of the file"
+puts "Please enter 'introspect' to print the program on your screen"
+
+
+option_input = gets.chomp.downcase
+
+
+if option_input == 'save'
+	File.open("hellofromruby.txt", "w") {|file|file.puts ("#{@ask_for_input}")}	
 end
+
+if option_input == 'read'
+	File.open("hellofromruby.txt", "r") {|file| puts file.read}
+end
+
+if option_input == 'introspect'
+	i =0
+	File.open(File.basename(__FILE__), "r").each do |line|
+		puts "#{i+=1} #{line}"
+	end
+end
+
+
 
